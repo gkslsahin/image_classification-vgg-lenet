2d070821beeb36346f565c625512521f1359505a
gkslsahin <gkslshn26@gmail.com>
2018-12-30 13:49:35 +0300

    added lenet

---
+++ lenet_cifar10.py
@@ -0,0 +1,63 @@
+
+# import the necessary packages
+import lenet
+from keras.optimizers import SGD
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import classification_report
+from keras.datasets import cifar10
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.datasets import cifar10
+from keras import backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+
+# grab dataset
+print("loading cifar-10 data...")
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+
+
+trainX = trainX.astype("float") / 255.0
+testX = testX.astype("float") / 255.0
+
+
+
+# convert the labels from integers to vectors
+le = LabelBinarizer()
+trainY = le.fit_transform(trainY)
+testY = le.transform(testY)
+
+labelNames = ["airplane", "automobile", "bird", "cat", "deer",
+"dog", "frog", "horse", "ship", "truck"]
+
+# initialize the optimizer and model
+print("compiling model...")
+opt = SGD(lr=0.01)
+model = lenet.LeNet.build(width=32, height=32, depth=3, classes=10)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# train 
+print("[training network...")
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+	batch_size=128, epochs=20, verbose=1)
+
+# evaluate the network
+print("evaluating network...")
+predictions = model.predict(testX, batch_size=128)
+print(classification_report(testY.argmax(axis=1),
+	predictions.argmax(axis=1),
+	target_names=labelNames))
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 20), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 20), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 20), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 20), H.history["val_acc"], label="val_acc")
+plt.title("train loss and accuracy")
+plt.xlabel("epoch")
+plt.ylabel("loss/accuracy")
+plt.legend()
+plt.show()
